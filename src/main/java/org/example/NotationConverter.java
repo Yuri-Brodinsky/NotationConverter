67b2ddca64be4434c38ae6ecbb68b8c1f7a1db20
Yuri Brodinsky <ybrodinsk@gmail.com>
2021-09-16 19:22:51 +0300

    add some fixes

---
+++ src/main/java/org/example/NotationConverter.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NotationConverter {
+    private final Map<String,Integer> map;
+    private final Map<Integer,String> reverseMap;
+    public NotationConverter(){
+        map = Map.of(
+                "I",1,
+                "V",5,
+                "X",10,
+                "L",50,
+                "C",100,
+                "D",500,
+                "M",1000
+        );
+        reverseMap = new HashMap<>();
+        map.entrySet().forEach(entry->reverseMap.put(entry.getValue(), entry.getKey()));
+
+    }
+    private int getValue(String key) throws InvalidValueException {
+        if(!map.containsKey(key))
+            throw new InvalidValueException("String must contain only valid roman numerals [I,V,X,L,C,D,M].");
+        return map.get(key);
+    }
+    private List<Integer> convertToIntList(String number) throws InvalidValueException {
+        String [] s = number.split("");
+        int value = getValue(s[0]);
+        int next = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<s.length;i++){
+            value = getValue(s[i]);
+            if(i+1<s.length){
+                next = getValue(s[i+1]);
+                if(value<next){
+                    if(next/value>10) throw new
+                            InvalidValueException("The preceding lesser numeral cannot be less than ten times.");
+                    else {
+                        list.add(next-value);
+                        i++;
+                    }
+                }
+                else list.add(value);
+            }
+            else list.add(value);
+        }
+        return list;
+    }
+    private int getResult(List<Integer> list) throws InvalidValueException {
+        int n = list.size();
+        int prev = list.get(0);
+        int res = prev;
+        int cur;
+        int limit = 1;
+        for(int i = 1;i<n;i++){
+            cur = list.get(i);
+            if(cur==prev) limit++;
+            else limit = 1;
+            if(cur > prev|| reverseMap.containsKey(cur+prev)||limit>3) throw
+                    new InvalidValueException("The order of the numerals is violated.");
+            if(limit==2&&! reverseMap.containsKey(cur)) throw
+                    new InvalidValueException("The order of the numerals is violated.");
+            prev = cur;
+            res = res + prev;
+        }
+        return res;
+    }
+    public int toArabic(String romanNotation) throws InvalidValueException {
+        return getResult(convertToIntList(romanNotation));
+    }
+}
+
---
+++ src/test/java/org/example/NotationConverterTest.java
@@ -0,0 +1,100 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class NotationConverterTest {
+    private static NotationConverter converter;
+    @BeforeAll
+    public static void init(){
+        converter = new NotationConverter();
+    }
+    @Test
+    public void shouldBe3() throws InvalidValueException {
+        int res = converter.toArabic("III");
+        Assertions.assertEquals(3,res);
+    }
+    @Test
+    public void shouldBe12() throws InvalidValueException {
+        int res = converter.toArabic("XII");
+        Assertions.assertEquals(12,res);
+    }
+    @Test
+    public void shouldBe59() throws InvalidValueException {
+        int res = converter.toArabic("LIX");
+        Assertions.assertEquals(59,res);
+    }
+    @Test
+    public void shouldBe1990() throws InvalidValueException {
+        int res = converter.toArabic("MCMXC");
+        Assertions.assertEquals(1990,res);
+    }
+    @Test
+    public void shouldThrowExceptionCaseOne() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("1"));
+        Assertions.assertEquals("String must contain only valid roman numerals [I,V,X,L,C,D,M].",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseTwo() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("VI1"));
+        Assertions.assertEquals("String must contain only valid roman numerals [I,V,X,L,C,D,M].",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseThree() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("A"));
+        Assertions.assertEquals("String must contain only valid roman numerals [I,V,X,L,C,D,M].",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseFour() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic(""));
+        Assertions.assertEquals("String must contain only valid roman numerals [I,V,X,L,C,D,M].",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseFive() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("MXM"));
+        Assertions.assertEquals("The preceding lesser numeral cannot be less than ten times.",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseSix() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("IIV"));
+        Assertions.assertEquals("The order of the numerals is violated.",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseSeven() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("XIXI"));
+        Assertions.assertEquals("The order of the numerals is violated.",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseEight() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("IXIX"));
+        Assertions.assertEquals("The order of the numerals is violated.",
+                thrown.getMessage());
+    }
+    @Test
+    public void shouldThrowExceptionCaseNine() throws InvalidValueException {
+        Throwable thrown = Assertions.assertThrowsExactly(
+                InvalidValueException.class,()->converter.toArabic("VIIII"));
+        Assertions.assertEquals("The order of the numerals is violated.",
+                thrown.getMessage());
+    }
+
+
+
+
+}
